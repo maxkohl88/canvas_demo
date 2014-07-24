de1be3c971dabbdd3bf6a2ca946a82186c24199c
Max Kohl <maxkohl810@gmail.com>
2014-07-24 12:30:57 -0400

    everything works except for drawing

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -10,6 +10,7 @@ canvasApp = {
     var context = canvas.getContext('2d');
     var imageObj = new Image();
 
+    //draw image and resize
     imageObj.onload = function(){
       canvas.width = 500;
       canvas.height = 500;
@@ -28,11 +29,42 @@ canvasApp = {
       canvas.width = 500;
       canvas.height = 500;
       context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height, 0, 0, canvas.width, canvas.height);
-      // debugger;
       // $urlInput.clear();
 
       event.preventDefault();
     });
+
+    var mouse = {x: 0, y: 0};
+ 
+    /* Mouse Capturing Work */
+    canvas.on('mousemove', function(e) {
+      mouse.x = e.pageX - this.offsetLeft;
+      mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+    /* Drawing on Paint App */
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'blue';
+     
+    canvas.addEventListener('mousedown', function(e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+     
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+     
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+     
+    var onPaint = function() {
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    };
+
     event.preventDefault();
-  }
+  },
+
 };
